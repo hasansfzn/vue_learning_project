ca561158adbe65c8037817d1d7b6ea8365fd4413
Softzino <softzino@Hasans-Machine.local>
2023-04-28 12:53:34 +0600

    computed ref implemented for filtering

--- src/components/AssignmentList.vue
+++ src/components/AssignmentList.vue
@@ -1,12 +1,35 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+const assignments = ref([
+  { name: "Fininsh Project", completed: false },
+  { name: "Read DOcs", completed: false },
+  { name: "Turn In Project", completed: false },
+]);
+
+const completedAssignments = computed(() => {
+  return assignments.value.filter((asg) => asg.completed === true);
+});
+</script>
 
 <template>
   <section>
-    <h2 class="text-3xl text-gray-700 font-bold mb-5">Assignment</h2>
+    <h2 class="text-3xl py-3 font-bold">Assignment</h2>
     <ul>
-      <li>Finish Project</li>
-      <li>Read Docs</li>
-      <li>Turn in Project</li>
+      <li v-for="assignment in assignments" :key="assignment.name">
+        <label>
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.completed" />
+        </label>
+      </li>
+    </ul>
+    <h3 class="mt-4 text-gray font-bold text-2xl">Completed Assignments</h3>
+    <p v-if="completedAssignments.length === 0">
+      No Assignments are completed.
+    </p>
+    <ul>
+      <li v-for="asg in completedAssignments" :key="asg.name">
+        {{ asg.name }}
+      </li>
     </ul>
   </section>
 </template>
