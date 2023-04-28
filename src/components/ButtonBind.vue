5255791a8d37e423e3e6328671d777964e29d854
Softzino <softzino@Hasans-Machine.local>
2023-04-28 11:03:35 +0600

    project initialized with git

---
+++ src/components/ButtonBind.vue
@@ -0,0 +1,18 @@
+<script setup>
+const buttonClasses = "text-green";
+</script>
+
+<template>
+  <div class="button_container">
+    <button :class="buttonClasses">Click Me</button>
+  </div>
+</template>
+
+<style>
+.text-red {
+  color: red;
+}
+.text-green {
+  color: green;
+}
+</style>
