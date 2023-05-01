1285330efa25db621d549bcd528a9b94830c05b4
Softzino <softzino@Hasans-Machine.local>
2023-05-01 14:00:18 +0600

    components are added separetely

---
+++ src/components/Assignments/AssignmentListCmp.vue
@@ -0,0 +1,24 @@
+<template>
+  <Assignment :assignment="inProgress" title="In Progress" />
+  <Assignment :assignment="completed" title="Completed" />
+</template>
+
+<script setup>
+import Assignment from "./Assignment.vue";
+import { ref, computed } from "vue";
+const assignments = ref([
+  { name: "Fininsh Project", completed: false },
+  { name: "Read DOcs", completed: false },
+  { name: "Turn In Project", completed: false },
+]);
+
+const completed = computed(() => {
+  return assignments.value.filter((asg) => asg.completed === true);
+});
+
+const inProgress = computed(() => {
+  return assignments.value.filter((asg) => !asg.completed);
+});
+</script>
+
+<style></style>
--- src/components/Assignments/Assignments.vue
+++ src/components/Assignments/Assignments.vue
@@ -1,46 +1,11 @@
 <script setup>
-import { ref, computed } from "vue";
-const assignments = ref([
-  { name: "Fininsh Project", completed: false },
-  { name: "Read DOcs", completed: false },
-  { name: "Turn In Project", completed: false },
-]);
-
-const completedAssignments = computed(() => {
-  return assignments.value.filter((asg) => asg.completed === true);
-});
-
-const tasksInProgress = computed(() => {
-  return assignments.value.filter((asg) => !asg.completed);
-});
+import AssignmentListCmp from "./AssignmentListCmp.vue";
 </script>
 
 <template>
   <section>
-    <h2 class="text-3xl py-3 font-bold">Assignment</h2>
-    <p v-show="!tasksInProgress.length" class="my-2 text-green-500">
-      All Assignments are done for now.
-    </p>
-    <ul>
-      <li v-for="assignment in tasksInProgress" :key="assignment.name">
-        <label>
-          {{ assignment.name }}
-          <input type="checkbox" v-model="assignment.completed" />
-        </label>
-      </li>
-    </ul>
-    <h3 class="mt-4 text-gray font-bold text-2xl">Completed Assignments</h3>
-    <p class="my-3" v-if="completedAssignments.length === 0">
-      No Assignments are completed yet.
-    </p>
-    <ul>
-      <li v-for="asg in completedAssignments" :key="asg.name">
-        <label
-          >{{ asg.name }}
-          <input type="checkbox" v-model="asg.completed" />
-        </label>
-      </li>
-    </ul>
+    <h2 class="text-3xl py-3 font-bold">Assignments to Do</h2>
+    <AssignmentListCmp />
   </section>
 </template>
 
