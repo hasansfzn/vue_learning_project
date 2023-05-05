b7b05e28395cee951823078bcf9d99f834076e23
Softzino <softzino@Hasans-Machine.local>
2023-05-05 14:44:41 +0600

    data filtered with button of categories

--- src/components/Assignments/AssignmentListCmp.vue
+++ src/components/Assignments/AssignmentListCmp.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { computed, ref } from "vue";
+import Assignment from "./Assignment.vue";
+const props = defineProps({
+  assignments: Array,
+  title: String,
+});
+
+const tags = computed(() => {
+  return ["All", ...new Set(props.assignments.map((asg) => asg.tag))];
+});
+
+const selectedTag = ref("All");
+
+// console.log(props.assignments);
+
+const selectedAssignments = computed(() => {
+  if (selectedTag.value == "All") {
+    return props.assignments;
+  }
+  return props.assignments.filter((asg) => asg.tag === selectedTag.value);
+});
+
+console.log("selected Assignments: ", selectedAssignments.value);
+</script>
+
 <template>
   <section>
     <h3 class="text-green-400">{{ title }}</h3>
@@ -8,9 +34,13 @@
         v-if="assignments.length"
       >
         <button
-          :class="{ 'px-2 py-1 my-2': true }"
+          :class="[
+            { 'px-2 py-1 my-2': true },
+            { 'border border-blue-400': selectedTag === tag },
+          ]"
           v-for="tag in tags"
           :key="tag"
+          @click="selectedTag = tag"
         >
           {{ tag }}
         </button>
@@ -25,24 +55,11 @@
     >
       <Assignment
         :assignment="assignment"
-        v-for="assignment in assignments"
+        v-for="assignment in selectedAssignments"
         :key="assignment.name"
       />
     </ul>
   </section>
 </template>
-
-<script setup>
-import { computed } from "vue";
-import Assignment from "./Assignment.vue";
-const props = defineProps({
-  assignments: Array,
-  title: String,
-});
-
-const tags = computed(() => {
-  return new Set(props.assignments.map((asg) => asg.tag));
-});
-</script>
 
 <style></style>
