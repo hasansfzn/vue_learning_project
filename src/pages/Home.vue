80c4f7298cfd312c924a74492e7ca222c9dd4707
Softzino <softzino@Hasans-Machine.local>
2023-05-11 17:32:03 +0600

    added transition

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -49,7 +49,7 @@ const addAnAssignment = (name, tag) => {
     <div class="flex justify-center gap-4">
       <AssignmentListCmp
         :class="{
-          'shadow shadow-blue-500/40 rounded p-4 m-1 w-96': true,
+          'shadow shadow-blue-500/40 rounded-lg  p-4 m-1 w-96 border border-gray-600 bg-gray-700': true,
         }"
         :assignments="inProgress"
         title="In Progress Assignments"
