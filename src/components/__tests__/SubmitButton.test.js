b6eb889fb4c77d9181167f1a35d923fb3eae1ea2
hasansfzn <hasan@softzino.com>
2023-05-16 10:25:28 +0600

    submit button test added

--- src/components/__tests__/SubmitButton.test.js
+++ src/components/__tests__/SubmitButton.test.js
@@ -1,9 +1,17 @@
 import { describe, it, expect, test } from "vitest";
 
-const sum = (a, b) => {
-  return a + b;
+const sum = (...nums) => {
+  return nums.reduce((a, b) => a + b, 0);
 };
 
 test("1+1", () => {
   expect(sum(1, 1)).toEqual(2);
+});
+
+test("3 NUMBERS", () => {
+  expect(sum(1, 1, 10)).toEqual(12);
+});
+
+test("1+1", () => {
+  expect(sum(1, 1, 100, 100)).toEqual(202);
 });
