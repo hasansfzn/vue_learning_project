6b055de69ff1d2f72c7a345f24661f52316bc62c
hasansfzn <hasan@softzino.com>
2023-05-16 11:32:56 +0600

    tested code with toMatchSnapshot() function

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -16,10 +16,12 @@ describe("Counter", () => {
   it("Should Increment", async () => {
     await wrapper.find("#increment").trigger("click");
     expect(wrapper.html()).toContain("Counter is : 1");
+    // expect(wrapper.find("#increment")).toMatchSnapshot();
   });
 
   it("Should Increment", async () => {
     await wrapper.find("#decrement").trigger("click");
     expect(wrapper.html()).toContain("Counter is : 0");
+    // expect(wrapper.find("#increment")).toMatchSnapshot();
   });
 });
