b427bad8e786d7dc26f5150e6f10073f42bc8607
hasansfzn <hasan@softzino.com>
2023-05-17 12:54:14 +0600

    used onTabPressed function

--- src/views/TextArea.vue
+++ src/views/TextArea.vue
@@ -1,33 +1,50 @@
 <script setup>
 import { onMounted, ref } from "vue";
 
-const textarea = ref(null);
-onMounted(() => {
-  textarea.value.addEventListener("keydown", (e) => {
-    let t = textarea.value;
+// const textarea = ref(null);
 
-    //when tab pressed
-    if (e.keyCode === 9) {
-      //get caret position/selection
-      let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd;
+const onTabPressed = (e) => {
+  let textArea = e.target;
 
-      //set textarea value to: text before caret + tab + text after caret
-      t.value = val.substring(0, start) + "\t" + val.substring(end);
+  //get caret position/selection
+  let val = textArea.value,
+    start = textArea.selectionStart,
+    end = textArea.selectionEnd;
 
-      //put caret at the right position again
-      t.selectionStart = t.selectionEnd = start + 1;
+  //set textarea value tp : text before caret + tab + text after caret
+  textArea.value = val.substring(0, start) + "\t" + val.substring(end);
 
-      e.preventDefault();
-    }
-  });
-});
+  //put caret at the right position again
+  textArea.selectionStart = textArea.selectionEnd = start + 1;
+};
+// onMounted(() => {
+//   textarea.value.addEventListener("keydown", (e) => {
+//     let t = textarea.value;
+
+//     //when tab pressed
+//     if (e.keyCode === 9) {
+//       //get caret position/selection
+//       let val = t.value,
+//         start = t.selectionStart,
+//         end = t.selectionEnd;
+
+//       //set textarea value to: text before caret + tab + text after caret
+//       t.value = val.substring(0, start) + "\t" + val.substring(end);
+
+//       //put caret at the right position again
+//       t.selectionStart = t.selectionEnd = start + 1;
+
+//       e.preventDefault();
+//     }
+//   });
+// });
 </script>
 
 <template>
   <div class="m-3 p-2 shadow shadow-gray-300">
-    <textarea ref="textarea" class="w-96 h-60">Hi THere</textarea>
+    <textarea class="w-96 h-60" @keydown.tab.prevent="onTabPressed">
+Hi THere</textarea
+    >
   </div>
 </template>
 
