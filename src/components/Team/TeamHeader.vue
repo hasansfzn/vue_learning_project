e175ea45e45402a0b364225ca3f4caabb24ba59d
hasansfzn <hasan@softzino.com>
2023-05-18 17:40:58 +0600

    team dashboard project started at '/team' route

---
+++ src/components/Team/TeamHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+  defineProps({
+    team: Object,
+  });
+</script>
+
+<template>
+  <header class="flex justify-between mt-8 pt-3">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="team.members.length === team.spots"
+      >
+        Add Member ( {{ team.spots - team.members.length }} Spots Left)
+      </button>
+    </div>
+    <div>
+      <h3 class="inline-flex items-center text-3xl relative">
+        <img
+          src=""
+          alt="Smiley"
+          class="mr-2"
+        />
+        Smiley Team
+        <div
+          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
+        >
+          {{ team.spots }}
+        </div>
+      </h3>
+    </div>
+  </header>
+</template>
+
+<style></style>
---
+++ src/components/Team/TeamMembersTable.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import TeamMember from "./TeamMember.vue";
+  defineProps({
+    team: Object,
+  });
+</script>
+
+<template>
+  <table class="table-fixed border-spacing-2 border-separate">
+    <thead>
+      <th class="text-left px-6 py-2">Name</th>
+      <th class="text-left px-6 py-2">Email</th>
+      <th class="text-left px-6 py-2">Status</th>
+    </thead>
+    <tbody>
+      <!-- <h2>{{ team.members.length }}</h2> -->
+      <TeamMember
+        v-for="member in team.members"
+        :key="member.id"
+        :member="member"
+      />
+    </tbody>
+  </table>
+</template>
+
+<style></style>
