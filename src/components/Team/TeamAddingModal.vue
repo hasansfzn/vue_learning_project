952c5d76e55e05ef426580c5e6c61d91261bdc80
hasansfzn <hasan@softzino.com>
2023-05-22 12:29:41 +0600

    display modal as the last element of the body, it was achieved by using teleport

---
+++ src/components/Team/TeamAddingModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <Modal
+    :show="showModal"
+    @close="showModal = false"
+  >
+    <!-- <template #header>
+      <h2>Overwritten header of the modal</h2>
+    </template> -->
+    <template #default>
+      <form>
+        <div class="flex gap-2">
+          <input
+            type="email"
+            class="flex-1 p-2"
+            placeholder="Enter Email..."
+          />
+          <button>Add</button>
+        </div>
+      </form>
+    </template>
+  </Modal>
+</template>
+
+<script setup>
+  import Modal from "./Modal.vue";
+</script>
+
+<style scoped></style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import TeamMember from "../components/Team/TeamMember.vue";
+  // import TeamMember from "../components/Team/TeamMember.vue";
   import TeamHeader from "../components/Team/TeamHeader.vue";
   import TeamMembersTable from "../components/Team/TeamMembersTable.vue";
   import TeamFooter from "../components/Team/TeamFooter.vue";
@@ -31,27 +31,28 @@
   </div>
 
   <TeamFooter />
-
-  <Modal
-    :show="showModal"
-    @close="showModal = false"
-  >
-    <!-- <template #header>
+  <Teleport to="body">
+    <Modal
+      :show="showModal"
+      @close="showModal = false"
+    >
+      <!-- <template #header>
       <h2>Overwritten header of the modal</h2>
     </template> -->
-    <template #default>
-      <form>
-        <div class="flex gap-2">
-          <input
-            type="email"
-            class="flex-1 p-2"
-            placeholder="Enter Email..."
-          />
-          <button>Add</button>
-        </div>
-      </form>
-    </template>
-  </Modal>
+      <template #default>
+        <form>
+          <div class="flex gap-2">
+            <input
+              type="email"
+              class="flex-1 p-2"
+              placeholder="Enter Email..."
+            />
+            <button>Add</button>
+          </div>
+        </form>
+      </template>
+    </Modal>
+  </Teleport>
 </template>
 
 <style></style>
